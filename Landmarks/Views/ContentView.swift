134a3e06eb38b68d20e4e4da83937e4fb6019a6c
jballo <jballona5127@gmail.com>
2022-07-11 22:40:13 -0700

    Building Lists and Navigation
    - generate scrollable list that user can tap on
    - tap on segues onto a detailed view for a landmark

---
+++ Landmarks/Views/ContentView.swift
@@ -0,0 +1,22 @@
+//
+//  ContentView.swift
+//  Landmarks
+//
+//  Created by Jonathan Ballona Sanchez on 7/10/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        
+        LandmarkList()
+            
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
